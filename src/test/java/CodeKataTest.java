4917bb83ce57e98fccf9e2db9919898cb5098f6e
stephenterapak <steve@terapak.com>
2021-05-30 08:07:19 -0500

    Slight bit of clean up with removal of enum to more usage of OO objects.

--- src/test/java/CodeKataTest.java
+++ src/test/java/CodeKataTest.java
@@ -1,5 +1,6 @@
 import com.oracle.codekata.files.FileConsumer;
 import com.oracle.codekata.data.Data;
+import com.oracle.codekata.files.FootballFileConsumer;
 import com.oracle.codekata.files.WeatherFileConsumer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +22,19 @@ class CodeKataTest {
     void readWeatherFile() {
         try{
             FileConsumer fc = new WeatherFileConsumer();
+            ArrayList<Data> dList = fc.ReadFile();
+            Assertions.assertTrue(dList.size()>0);
+        }
+        catch(Exception e){
+            Assertions.fail();
+        }
+
+    }
+
+    @Test
+    void readFootballFile() {
+        try{
+            FileConsumer fc = new FootballFileConsumer();
             ArrayList<Data> dList = fc.ReadFile();
             Assertions.assertTrue(dList.size()>0);
         }
